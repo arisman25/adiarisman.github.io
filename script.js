6bff7cfba905d66c2d8b25151c9a929b4285bad3
arisman25 <42736874+arisman25@users.noreply.github.com>
2022-03-16 16:57:22 +0700

    push 1

---
+++ script.js
@@ -0,0 +1,10 @@
+var tombolMenu = document.getElementsByClassName('menu-dropdown')[0];
+var menu = document.getElementsByClassName('menu')[0];
+
+tombolMenu.onclick = function() {
+    menu.classList.toggle('active');
+}
+
+menu.onclick = function() {
+    menu.classList.toggle('active');
+}
